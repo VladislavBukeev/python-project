fcb66ba4ac9e5f49a4d4263fa124b5f4f831cc93
Vladislav Bukreev <36801634-vladfootgamer@users.noreply.replit.com>
2024-10-04 16:44:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#a  = input()
+#if len(a) > 8 and len(a) < 20:
+#  print('Принято')
+#elif len(a) < 8:
+#  print('Слишком маленький')
+#elif len(a) > 20:
+#  print('Слишком большой')
+#print(a.upper())
+#print(a.lower())
+#print(a.count('о', 4, 10))
+#print(a.replace('о', 'а', 3))
+#print(a[6:10])
+#print(a[::-1])
+a = int(input())
+while a != 0:
+    print(a + 5)
+    a = int(input())
